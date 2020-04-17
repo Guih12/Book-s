67af499cc302da09111a57edb0817d1eda371e05
Your Name <you@example.com>
2020-04-16 21:41:04 -0300

    modulo de livro

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,6 +3,7 @@ import { BookService } from './book.service';
 import {BookController} from './book.controller';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import {Book} from './book.entity';
+import {User} from '../users/user/user.entity';
 @Module({
   imports:[TypeOrmModule.forFeature([Book])],
   providers: [BookService],
